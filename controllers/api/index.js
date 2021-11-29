8f1e35aa0e5749c21030f29a8cf450b001c77d2f
DoodleBob <riley.foust18@gmail.com>
2021-11-29 18:09:35 -0500

    added handlebars pages and router logic

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,7 @@
 const router = require("express").Router();
 
+const heroRoutes = require("./superhero-routes");
+
+router.use("/hero", heroRoutes);
+
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,4 +8,8 @@ router.get('/universes', (req, res) => {
   res.render('universes');
 });
 
+router.get('/all-universes', (req, res) => {
+  res.render('all-universes');
+});
+
 module.exports = router;
