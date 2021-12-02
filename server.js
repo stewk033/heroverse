2031b864f2d00bd5e93c9fa90dabf0db3b6a3738
DoodleBob <riley.foust18@gmail.com>
2021-12-02 18:26:40 -0500

    fixed logo

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, '/assets')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
