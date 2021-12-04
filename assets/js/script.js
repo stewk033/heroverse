a5f95d8aa42fefaced714accbb2470f1f5ed4f79
christi marchetti <christimarchetti@gmail.com>
2021-12-04 09:19:11 -0500

    regenerated page-125 refactored page 5 grid js for universe icon buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,37 +6,13 @@ const dcComics = document.getElementById('dc-comics');
 const darkHorse = document.getElementById('dark-horse');
 const all = document.getElementById('all');
 
-const getHeroInfo = function(heroName) {
-    console.log("getHeroInfo function was called");
-
-    fetch("https://superheroapi.com/api/10209423852668692/search/name" + heroName, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "superheroapi.com",
-            "x-rapidapi-key": "10209423852668692"
-}
-    })
-    // array logged in the console
-//     .then(response => {
-//         response.json()
-//             .then(function(data) {
-//             console.log(data);
-//             displayPlayerInfo(data, playerName);
-//         });
-// })
-//     .catch(err => {
-//         console.error(err);
-//     });
-
-};
 
 
 
-
-starWars.addEventListener('click')
-marvel.addEventListener('click')
-hasbro.addEventListener('click')
-disney.addEventListener('click')
-dcComics.addEventListener('click')
-darkHorse.addEventListener('click')
-all.addEventListener('click')
+starWars.addEventListener('click', displayCharacters)
+marvel.addEventListener('click',displayCharacters)
+hasbro.addEventListener('click', displayCharacters)
+disney.addEventListener('click', displayCharacters)
+dcComics.addEventListener('click', displayCharacters)
+darkHorse.addEventListener('click', displayCharacters)
+all.addEventListener('click', displayCharacters)
